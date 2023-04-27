fad30eff03e4a3c97feec9ba2eee9d8b1ae99910
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2023-04-26 22:38:32 -0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,4 @@
+<form name="registration" onsubmit="return validateForm()">
+    <!-- Form fields -->
+    <button type="submit">Register</button>
+</form>
---
+++ script.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    var email = document.forms["registration"]["email"].value;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Please enter a valid email address");
+        return false;
+    }
+}
